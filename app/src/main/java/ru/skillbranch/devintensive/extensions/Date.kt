352f58617eb94d9540166cd5c51b769cbd2521c0
DaniilShcherbakov <Charel@bk.ru>
2020-04-09 14:01:16 +0300

    TimeUtils.pluar complete

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -1,6 +1,5 @@
 package ru.skillbranch.devintensive.extensions
 
-import java.sql.Time
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -13,14 +12,14 @@ fun Date.format(pattern:String = "HH:mm:ss dd:MM:yy"):String{
     return dateFormat.format(this)
 }
 
-fun Date.add(value:Int, units:TimeUnites = TimeUnites.SECOND): Date{
+fun Date.add(value:Int, units:TimeUnits = TimeUnits.SECOND): Date{
     var time = this.time
 
     time += when(units){
-        TimeUnites.SECOND -> value * SECOND
-        TimeUnites.MINUTE-> value * MINUTE
-        TimeUnites.HOUR-> value * HOUR
-        TimeUnites.DAY -> value * DAY
+        TimeUnits.SECOND -> value * SECOND
+        TimeUnits.MINUTE-> value * MINUTE
+        TimeUnits.HOUR-> value * HOUR
+        TimeUnits.DAY -> value * DAY
 
     }
     this.time = time
@@ -35,11 +34,11 @@ fun Date.humanizeDiff(date: Date= Date()): String {
         in 0..1 -> "только что"
         in 2..45 -> "несколько секунд назад"
         in 46..75 -> "минуту назад"
-        in 76..45*60 ->"N минут назад"
+        in 76..45*60 ->"${TimeUnits.MINUTE.plural((interval/60).toInt())} назад"
         in (45*60+1)..75*60 ->"час назад"
-        in (75*60+1)..22*3600 ->"N часов назад"
+        in (75*60+1)..22*3600 ->"${TimeUnits.HOUR.plural((interval/3600).toInt())} назад"
         in (22*3600+1)..26*3600 -> "день назад"
-        in (26*3600+1)..360*3600*24 ->"N дней назад"
+        in (26*3600+1)..360*3600*24 ->"${TimeUnits.DAY.plural((interval/3600/24).toInt())} назад"
         else -> "более года назад"
     }
 return "$message"
@@ -49,9 +48,71 @@ return "$message"
 
 
 
-enum class TimeUnites{
+
+
+enum class TimeUnits{
     SECOND,
     MINUTE,
     HOUR,
-    DAY
+    DAY;
+
+        fun plural(int:Int):String{
+            var result:String =""
+            val rem100: Int = int%100
+            val rem: Int = int%10
+            if ( rem100 in 10..19){
+                result = when(this.name){
+                    SECOND.name -> "$int секунд"
+                    MINUTE.name -> "$int минут"
+                    HOUR.name -> "$int часов"
+                    DAY.name -> "$int дней"
+                    else ->""
+                }
+
+            } else {
+            when(rem){
+                0 ->{
+                    result = when(this.name){
+                        SECOND.name -> "$int секунд"
+                        MINUTE.name -> "$int минут"
+                        HOUR.name -> "$int часов"
+                        DAY.name -> "$int дней"
+                        else ->""
+                    }
+                }
+                1 ->{
+                    result = when(this.name){
+                        SECOND.name -> "$int секунда"
+                        MINUTE.name -> "$int минута"
+                        HOUR.name -> "$int час"
+                        DAY.name -> "$int день"
+                        else ->""
+                    }
+                }
+                in 2..4 ->{
+                    result = when(this.name){
+                        SECOND.name -> "$int секунды"
+                        MINUTE.name -> "$int минуты"
+                        HOUR.name -> "$int часа"
+                        DAY.name -> "$int дня"
+                        else ->""
+                    }
+                }
+                in 5..9 ->{
+                    result = when(this.name){
+                        SECOND.name -> "$int секунд"
+                        MINUTE.name -> "$int минут"
+                        HOUR.name -> "$int часов"
+                        DAY.name -> "$int дней"
+                        else ->""
+                    }
+                }
+            }
+            }
+
+
+          return result
+        }
+
+
 }
