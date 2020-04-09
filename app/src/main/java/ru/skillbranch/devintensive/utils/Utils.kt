5289783dc32f16b5b2807703279cccc84c434e08
DaniilShcherbakov <Charel@bk.ru>
2020-04-09 12:15:06 +0300

    transliteration complete (7)

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -14,7 +14,70 @@ object Utils {
     }
 
     fun transliteration(payload: String, divider:String = " "): String {
-    return  ""
+        var result:String =""
+        if (!payload.trim().isNullOrEmpty()){
+            val parts: List<String> = payload.trim().split(" ")
+            var counterWords = 0
+            parts.forEach {
+                if (counterWords>0) {
+                result+=divider
+                }
+                for (i in it.indices){
+                    if (it[i].toUpperCase() == it[i]){
+                        var str:String =translitChar(it[i].toLowerCase())
+
+                        result +=str.replaceRange(0..0,str[0].toUpperCase().toString())
+                    }else{
+
+                    result +=translitChar(it[i])
+                    }
+                }
+                counterWords++
+
+            }
+        }
+    return  result
+    }
+
+    fun translitChar(char: Char):String{
+        var result:String = ""
+        result = when(char){
+            'а' -> "a"
+            'б' -> "b"
+            'в' -> "v"
+            'г' -> "g"
+            'д' -> "d"
+            'е' -> "e"
+            'ё' -> "e"
+            'ж' -> "zh"
+            'з' -> "z"
+            'и' -> "i"
+            'й' -> "i"
+            'к' -> "k"
+            'л' -> "l"
+            'м' -> "m"
+            'н' -> "n"
+            'о' -> "o"
+            'п' -> "p"
+            'р' -> "r"
+            'с' -> "s"
+            'т' -> "t"
+            'у' -> "u"
+            'ф' -> "f"
+            'х' -> "h"
+            'ц' -> "c"
+            'ч' -> "ch"
+            'ш' -> "sh"
+            'щ' -> "sh'"
+            'ъ' -> ""
+            'ы' -> "i"
+            'ь' -> ""
+            'э' -> "e"
+            'ю' -> "yu"
+            'я' -> "ya"
+            else ->char.toString()
+        }
+        return result
     }
 
     fun toInitials(firstName: String?, lastName: String?): String? {
