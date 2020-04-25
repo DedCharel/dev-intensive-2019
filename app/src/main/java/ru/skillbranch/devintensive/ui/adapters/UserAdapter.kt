de03f7dc0475612eae5b73c6deb1196d977210d6
DaniilShcherbakov <Charel@bk.ru>
2020-04-25 20:36:24 +0300

    create UserAdapter

--- app/src/main/java/ru/skillbranch/devintensive/ui/adapters/UserAdapter.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/adapters/UserAdapter.kt
@@ -3,9 +3,11 @@ package ru.skillbranch.devintensive.ui.adapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.item_user_list.*
 import ru.skillbranch.devintensive.R
 import ru.skillbranch.devintensive.models.data.UserItem
 
@@ -22,6 +24,25 @@ class UserAdapter(val listener: (UserItem) ->Unit): RecyclerView.Adapter<UserAda
     override fun getItemCount(): Int = items.size
 
     override fun onBindViewHolder(holder: UserViewHolder, position: Int) = holder.bind(items[position], listener)
+
+    fun updateData(data: List<UserItem>){
+        val diffCallback = object: DiffUtil.Callback(){
+            override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
+                items[oldItemPosition].id == data[newItemPosition].id
+
+            override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
+                items[oldItemPosition].hashCode() == data[newItemPosition].hashCode()
+
+            override fun getOldListSize(): Int = items.size
+
+            override fun getNewListSize(): Int = data.size
+        }
+
+        val diffResult = DiffUtil.calculateDiff(diffCallback)
+        items = data
+        diffResult.dispatchUpdatesTo(this)
+    }
+
     inner class UserViewHolder(convertView:View): RecyclerView.ViewHolder(convertView), LayoutContainer {
         override val containerView: View?
             get() = itemView
@@ -29,8 +50,17 @@ class UserAdapter(val listener: (UserItem) ->Unit): RecyclerView.Adapter<UserAda
         fun bind(user: UserItem, listener: (UserItem) -> Unit){
             if (user.avatar != null){
                 Glide.with(itemView)
-
+                    .load(user.avatar)
+                    .into(iv_avatar_user)
+            } else{
+                Glide.with(itemView).clear(iv_avatar_user)
+                iv_avatar_user.setInitials(user.initials ?: "??")
             }
+            sv_indicator.visibility = if (user.isOnline) View.VISIBLE else View.GONE
+            tv_user_name.text = user.fullName
+            tv_last_activity.text = user.lastActivity
+            iv_selected.visibility = if(user.isSelected) View.VISIBLE else View.GONE
+            itemView.setOnClickListener{listener.invoke(user)}
         }
 
     }
