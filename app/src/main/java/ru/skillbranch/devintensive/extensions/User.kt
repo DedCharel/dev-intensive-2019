83898a4a2509763b476acc154299ecbc8cebe97e
DaniilShcherbakov <Charel@bk.ru>
2020-04-27 16:23:26 +0300

    танцы с бубном 2 (не принимает структуру)

--- app/src/main/java/ru/skillbranch/devintensive/extensions/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/User.kt
@@ -1,10 +1,10 @@
 package ru.skillbranch.devintensive.extensions
 
 import ru.skillbranch.devintensive.models.data.User
-import ru.skillbranch.devintensive.models.UserView
+import ru.skillbranch.devintensive.utils.UserView
 import ru.skillbranch.devintensive.utils.Utils
 
-fun User.toUserView():UserView{
+fun User.toUserView(): UserView {
     var nickName = Utils.transliteration("$firstName $lastName")
     var initials = Utils.toInitials(firstName, lastName)
     var status = if(lastVisit == null) "Ещё ни разу не был" else if(isOnline) "online" else "Последний раз был ${lastVisit.humanizeDiff()}"
