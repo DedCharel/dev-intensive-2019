f859112d6ea434bce87fe4adc8b84172aa7d00f6
DaniilShcherbakov <Charel@bk.ru>
2020-04-27 15:49:33 +0300

    танцы с бубном (не принимает структуру)

--- app/src/main/java/ru/skillbranch/devintensive/models/ImageMessage.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/ImageMessage.kt
@@ -1,9 +1,9 @@
 package ru.skillbranch.devintensive.models
 
-import ru.skillbranch.devintensive.extensions.humanizeDiff
 import ru.skillbranch.devintensive.models.data.Chat
 import ru.skillbranch.devintensive.models.data.User
 import java.util.*
+
 
 class ImageMessage (
     id:String,
@@ -13,8 +13,4 @@ class ImageMessage (
     date: Date = Date(),
     isReaded:Boolean = false,
     var image:String
-) : BaseMessage(id, from, chat, isIncoming, date, isReaded)
-//{
-//    override fun formatMessage(): String = "${from?.firstName}" +
-//            " ${if(isIncoming) "получил" else "отправил"} изображение \"$image\" ${date.humanizeDiff()}"
-//}
+) : BaseMessage(id, from, chat, isIncoming, date,isReaded)
--- app/src/main/java/ru/skillbranch/devintensive/models/TextMessage.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/TextMessage.kt
@@ -1,9 +1,9 @@
 package ru.skillbranch.devintensive.models
 
-import ru.skillbranch.devintensive.extensions.humanizeDiff
 import ru.skillbranch.devintensive.models.data.Chat
 import ru.skillbranch.devintensive.models.data.User
 import java.util.*
+
 
 class TextMessage(
     id: String,
@@ -14,7 +14,4 @@ class TextMessage(
     isReaded:Boolean = false,
     var text: String?
 ) : BaseMessage(id, from, chat, isIncoming, date, isReaded)
-//{
-//    override fun formatMessage(): String = "${from?.firstName}" +
-//            " ${if(isIncoming) "получил" else "отправил"} сообщение \"$text\" ${date.humanizeDiff()}"
-//}
+
